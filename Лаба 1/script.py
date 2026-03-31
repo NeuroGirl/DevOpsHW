51ab05825fa065ccac8ae33fb2a1e1d9166f3e9a
NeuroGirl <vesus0206@gmail.com>
2026-03-31 21:48:21 +0300

    Create script.py

---
+++ Лаба 1/script.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import json
+import ctypes
+
+libc = ctypes.CDLL('libc.so.6', use_errno=True)
+
+CLONE_NEWUTS = 0x04000000
+CLONE_NEWPID = 0x20000000
+CLONE_NEWNS = 0x00020000
+
+
+def limit_resources(container_id):
+    cpath = f"/sys/fs/cgroup/{container_id}"
+    os.makedirs(cpath, exist_ok=True)
+    with open(f"{cpath}/memory.max", 'w') as f: f.write("256M")
+    with open(f"{cpath}/cgroup.procs", 'w') as f: f.write(str(os.getpid()))
+
+
+def setup_overlay(container_id):
+    base = f"/var/lib/my-tool/{container_id}"
+    lower = "/tmp/alpine/rootfs"
+    upper, work, merged = f"{base}/upper", f"{base}/work", f"{base}/merged"
+    for d in [upper, work, merged]: os.makedirs(d, exist_ok=True)
+
+    opts = f"lowerdir={lower},upperdir={upper},workdir={work}"
+    libc.mount(b"overlay", merged.encode(), b"overlay", 0, opts.encode())
+    return merged
+
+
+def child_process(container_id):
+    config = json.load(open('config.json'))
+
+    limit_resources(container_id)
+
+    hostname = config.get('hostname', 'container')
+    libc.sethostname(hostname.encode(), len(hostname))
+
+    merged_path = setup_overlay(container_id)
+
+    proc_path = os.path.join(merged_path, 'proc')
+    os.makedirs(proc_path, exist_ok=True)
+    libc.mount(b"proc", proc_path.encode(), b"proc", 0, None)
+
+    os.chroot(merged_path)
+    os.chdir("/")
+
+    args = config['process']['args']
+    os.execvp(args[0], args)
+
+
+def main():
+    try:
+        libc.unshare(CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS)
+    except Exception as error:
+        print(f"{error}")
+        return
+
+    pid = os.fork()
+    if pid == 0:
+        child_process(sys.argv[2])
+    else:
+        os.waitpid(pid, 0)
+        print("\n[Контейнер завершен]")
+
+
+if __name__ == "__main__":
+    main()
